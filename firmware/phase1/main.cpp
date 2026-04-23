7715c2d4786842bf6db295a086d97f6409aa1d0f
DaviPortoIzabel <davipizabel@gmail.com>
2026-04-23 14:36:15 -0300

    More things implemented in the code of firmware

--- firmware/phase1/main.cpp
+++ firmware/phase1/main.cpp
@@ -1,21 +1,48 @@
-int pin02 = 2;
-int pin04 = 4;
+int pin02 = 2; // LED pin
+int pin04 = 4; // Button pin
 
-void setup(){
+// Button is touched? Count the time
+unsigned long flowStartTime = 0;
+bool flowActive = false;
+
+// After 5 seconds, alarm the leak
+unsigned long leakThreshold = 5000;
+
+void setup() {
     pinMode(pin02, OUTPUT);
     pinMode(pin04, INPUT_PULLUP);
     Serial.begin(115200);
 }
 
-void loop(){
+void loop() {
     int sensorState = digitalRead(pin04);
-    if (sensorState == LOW){
-        digitalWrite(pin02, HIGH);
-        Serial.println("PULSED DETECT");
-    }
-    else{
+
+    if (sensorState == LOW) {
+        // If flow just started, save the start time
+        if (!flowActive) {
+            flowActive = true;
+            flowStartTime = millis();
+        }
+
+        // Calculate how long the flow has been active
+        unsigned long flowDuration = millis() - flowStartTime;
+
+        if (flowDuration < leakThreshold) {
+            digitalWrite(pin02, HIGH);
+            Serial.println("Normal Flow");
+        } else {
+            Serial.println("Leak Detected");
+
+            for (int i = 0; i < 4; i++) {
+                digitalWrite(pin02, HIGH);
+                delay(100);
+                digitalWrite(pin02, LOW);
+                delay(100);
+            }
+        }
+
+    } else {
+        flowActive = false;
         digitalWrite(pin02, LOW);
     }
-
-    delay(100);
 }
